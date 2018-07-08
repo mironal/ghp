5515a9b1ef2369c59aa34590e91b8b348166eefc
mironal <miro.gunp@gmail.com>
2018-07-08 22:15:19 +0900

    add get project

--- src/commands/projects/get.ts
+++ src/commands/projects/get.ts
@@ -1,11 +1,17 @@
-import { Command, flags } from "@oclif/command"
+import { flags } from "@oclif/command"
+import AuthCommand from "../../base"
+import { format, reporterFlag } from "../../github/reporter"
 
-export default class ProjectsGet extends Command {
+export default class ProjectsGet extends AuthCommand {
   static description = "describe the command here"
 
   static flags = {
     help: flags.help({ char: "h" }),
-    project: flags.string({ char: "p", description: "A project id." }),
+    project: flags.string({
+      char: "p",
+      required: true,
+    }),
+    ...reporterFlag,
   }
 
   static args = []
@@ -14,6 +20,20 @@ export default class ProjectsGet extends Command {
     const { flags } = this.parse(ProjectsGet)
     const { project } = flags
 
-    this.log(`get a project ${project}`)
+    const resp = await this.client.projects
+      .getProject({
+        id: project,
+        project_id: project,
+      })
+      .catch(this.error)
+
+    this.log(
+      format<{ [key: string]: string }>(resp.data, flags.reporter!, data => {
+        return Object.keys(data)
+          .filter(key => !["creator"].includes(key))
+          .map(key => `${key}: ${data[key]}`)
+          .join("\n")
+      }),
+    )
   }
 }
